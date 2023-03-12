3a4382e7ebe227b689311be12e02c89d51602cb1
ivan <mega.dima.r@mail.ru>
2023-03-12 23:50:45 +0300

    Hw2_task_RateLimitedPrinter

---
+++ src/HW_2/task_RateLimitedPrinter/RateLimitedPrinterTest.java
@@ -0,0 +1,11 @@
+package HW_2.task_RateLimitedPrinter;
+
+public class RateLimitedPrinterTest {
+    public static void main(String[] args) {
+        RateLimitedPrinter rateLimitedPrinter = new RateLimitedPrinter(1000);
+
+        for (int i = 0; i < 1_000_000_000; i++) {
+            rateLimitedPrinter.print(String.valueOf(i));
+        }
+    }
+}
